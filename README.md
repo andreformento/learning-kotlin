b3aec7f01f8db50beae88337c4caf93f2646c8f6
André Formento <andreformento.sc@gmail.com>
2021-07-03 10:08:20 -0300

    wip

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## References
 - https://spring.io/blog/2019/04/12/going-reactive-with-spring-coroutines-and-kotlin-flow
+- https://spring.io/guides/tutorials/spring-webflux-kotlin-rsocket/
+- https://github.com/tolkiana/liquibase-demo
--- myapp/src/main/kotlin/com/andreformento/myapp/MyappApplication.kt
+++ myapp/src/main/kotlin/com/andreformento/myapp/MyappApplication.kt
@@ -1,11 +1,24 @@
 package com.andreformento.myapp
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
 @SpringBootApplication
 class MyappApplication
 
 fun main(args: Array<String>) {
 	runApplication<MyappApplication>(*args)
+}
+
+data class User(val id: String, val firstname: String)
+
+interface CoroutineRepository : CoroutineCrudRepository<User, String> {
+
+	suspend fun findOne(id: String): User
+
+	fun findByFirstname(firstname: String): Flow<User>
+
+	suspend fun findAllByFirstname(id: String): List<User>
 }
