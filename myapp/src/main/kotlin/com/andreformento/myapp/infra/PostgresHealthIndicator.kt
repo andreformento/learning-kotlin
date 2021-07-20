55daaf56c3dadfa38e840a3b78a2228e1f224e48
André Formento <andreformento.sc@gmail.com>
2021-07-20 18:34:56 -0300

    readiness

--- myapp/src/main/kotlin/com/andreformento/myapp/infra/PostgresHealthIndicator.kt
+++ myapp/src/main/kotlin/com/andreformento/myapp/infra/PostgresHealthIndicator.kt
@@ -2,28 +2,31 @@ package com.andreformento.myapp.infra
 
 import com.andreformento.myapp.post.Post
 import com.andreformento.myapp.post.repository.Posts
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.stereotype.Component
 
 @Component
-class PostgresHealthIndicator(private val posts: Posts): HealthIndicator {
+class PostgresHealthIndicator(private val posts: Posts) : HealthIndicator {
+    // TODO this is the best way? I hope not!
     override fun health(): Health {
         return try {
-//            val posts = GlobalScope.async { posts.findAll() }
-            val a: Flow<Post> = runBlocking { posts.findAll() }
+            var firstPost: Post?
+            runBlocking(Dispatchers.Default) {
+                firstPost = posts.findAll().firstOrNull()
+            }
 
-//            println("posts ${posts.}")
-            Health.up().build();
+            if (firstPost == null) {
+                Health.down().withDetail("Cannot get any data from database", object {}).build()
+            } else {
+                println("firstPost $firstPost")
+                Health.up().build()
+            }
         } catch (e: Exception) {
-            Health.down().withDetail("Cannot connect to database", 503).build();
+            Health.down().withDetail("Cannot connect to database", e).build()
         }
     }
 }
