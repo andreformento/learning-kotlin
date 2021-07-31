ddffcf28f596693b32290b547a3d7f18dd823dd1
André Formento <andreformento.sc@gmail.com>
2021-07-31 16:52:32 -0300

    wip

---
+++ money/src/main/kotlin/organization/role/api/OrganizationRoleController.kt
@@ -0,0 +1,36 @@
+package com.andreformento.money.organization.role.api
+
+import com.andreformento.money.organization.role.OrganizationRoleCreated
+import com.andreformento.money.organization.role.OrganizationRoleCreation
+import com.andreformento.money.organization.role.OrganizationRoleFacade
+import com.andreformento.money.organization.role.toOrganizationRoleId
+import com.andreformento.money.organization.toOrganizationId
+import com.andreformento.money.user.toUserId
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+@RestController
+@RequestMapping("/organizations/{organization-id}/roles", produces = ["application/json"])
+class OrganizationRoleController(private val organizationRoleFacade: OrganizationRoleFacade) {
+
+    @PostMapping
+    suspend fun create(@RequestBody organizationRoleCreation: OrganizationRoleCreation): ResponseEntity<OrganizationRoleCreated> {
+        val organizationRoleCreated = organizationRoleFacade.create(organizationRoleCreation)
+        return ResponseEntity.created(URI.create("/organizations/${organizationRoleCreated.id}")).body(organizationRoleCreated)
+    }
+
+    @DeleteMapping("/{organization-id}")
+    suspend fun delete(
+        @PathVariable("organization-id") organizationId: String,
+        @PathVariable("organization-role-id") organizationRoleId: String): ResponseEntity<Any> {
+        val deletedCount = organizationRoleFacade.delete(
+            organizationRoleId = organizationRoleId.toOrganizationRoleId(),
+            organizationId = organizationId.toOrganizationId(),
+            userId = "TODO".toUserId(), // TODO get userID
+        )
+        println("$deletedCount organization roles deleted")
+        return ResponseEntity.noContent().build()
+    }
+
+}
