dd71858e13fac91d40c8a1d42b0c6d5fa987c843
André Formento <andreformento.sc@gmail.com>
2021-09-11 18:09:26 -0300

    Migrate web client test (#12)
    * migrate webclient test
    
    wip
    
    wip
    
    wip
    
    * wip
    
    * remove file
    
    * test body ok

--- money/src/test/kotlin/user/api/UserControllerTest.kt
+++ money/src/test/kotlin/user/api/UserControllerTest.kt
@@ -2,20 +2,23 @@ package com.andreformento.money.user.api
 
 import com.andreformento.money.user.security.UserCredentials
 import com.andreformento.money.user.security.api.UserSignupCreation
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.http.HttpStatus
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.test.web.reactive.server.returnResult
 import org.springframework.util.LinkedMultiValueMap
-import org.springframework.web.reactive.function.client.WebClient
 import kotlin.random.Random
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-internal class UserControllerTest {
 
-    @LocalServerPort
-    var serverPort: Int? = null
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UserControllerTest {
+
+    @Autowired
+    private lateinit var webClient: WebTestClient
 
     @Test
     fun `can obtain own user details when logged in`() {
@@ -23,44 +26,37 @@ internal class UserControllerTest {
         // TODO this is very wrong!!!
         val email = "new@example.com" + Random.nextInt()
 
-        val webClient = WebClient
-            .builder()
-            .baseUrl("http://localhost:${serverPort}")
-            .build()
-
         // act
-        val signupResponse = webClient
+        webClient
             .post()
             .uri("/user/auth/signup")
             .bodyValue(UserSignupCreation(name = "user test", email = email, password = "pw"))
             .exchange()
-            .block() ?: throw RuntimeException("Should have gotten a signup response")
-
-        assertThat(signupResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT)
+            .expectStatus().isNoContent
 
         val loginResponse = webClient
             .post()
             .uri("/user/auth/login")
             .bodyValue(UserCredentials(email, "pw"))
             .exchange()
-            .block() ?: throw RuntimeException("Should have gotten a login response")
-        val responseCookies = loginResponse.cookies()
-            .map { it.key to it.value.map { cookie -> cookie.value } }
-            .toMap()
+            .expectStatus().isOk
+            .returnResult<Any>()
 
-        val response = webClient
+        // TODO why it isn't working?
+        val xAuth = loginResponse
+            .responseHeaders
+            .getValuesAsList("Set-Cookie")[0]!!
+            .replaceFirst("X-Auth=", "")
+            .split(";")[0]
+
+        println("LinkedMultiValueMap(mapOf-> " + LinkedMultiValueMap(mapOf("X-Auth" to listOf(xAuth))))
+        webClient
             .get()
             .uri("/user")
-            .cookies { it.addAll(LinkedMultiValueMap(responseCookies)) }
+            .cookies { it.addAll(LinkedMultiValueMap(mapOf("X-Auth" to listOf(xAuth)))) }
             .exchange()
-            .block()
-
-        // assert
-        assertThat(response?.statusCode())
-            .isEqualTo(HttpStatus.OK)
-
-        assertThat(response?.bodyToMono(LoggedUserResponse::class.java)?.block())
-            .isEqualTo(LoggedUserResponse(name = "user test", email = email))
+            .expectStatus().isOk
+            .expectBody<LoggedUserResponse>().isEqualTo(LoggedUserResponse(name = "user test", email = email))
     }
 
 }
