24100ec79bac401be036737930659e0b788a0031
André Formento <andreformento.sc@gmail.com>
2021-10-14 22:02:48 -0300

    makefile

---
+++ search/src/test/kotlin/com/formento/search/IntegrationTest.kt
@@ -0,0 +1,68 @@
+package com.formento.search
+
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.impl.Http2SolrClient
+import org.apache.solr.client.solrj.response.SolrPingResponse
+import org.junit.ClassRule
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.testcontainers.containers.SolrContainer
+import org.testcontainers.utility.DockerImageName
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension::class)
+@DirtiesContext
+abstract class IntegrationTest {
+
+    companion object {
+
+        @ClassRule
+        val solrContainer = SolrContainer(DockerImageName.parse("solr:8.10.0"))
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun setProperties(registry: DynamicPropertyRegistry) {
+//            registry.add("spring.datasource.url", solrContainer::getJdbcUrl)
+//            registry.add("spring.datasource.username", solrContainer::getUsername)
+//            registry.add("spring.datasource.password", solrContainer::getPassword)
+        }
+
+        @BeforeAll
+        @JvmStatic
+        internal fun beforeAll() {
+            solrContainer.start()
+            val client: SolrClient = Http2SolrClient.Builder(
+                "http://${solrContainer.containerIpAddress}:${solrContainer.solrPort}/solr"
+            ).build()
+
+            val response: SolrPingResponse = client.ping("dummy")
+            println(response.elapsedTime)
+        }
+
+        @AfterAll
+        @JvmStatic
+        internal fun afterAll() {
+            solrContainer.close()
+        }
+    }
+
+    @Bean
+    fun mockSolrClient(): SolrClient =
+        Http2SolrClient.Builder("http://${solrContainer.containerIpAddress}:${solrContainer.solrPort}/solr").build()
+
+    @AfterEach
+    fun cleanUp() {
+    }
+
+}
---
+++ search/src/test/kotlin/com/formento/search/SearchRepositoryTest.kt
@@ -0,0 +1,13 @@
+package com.formento.search
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SearchRepositoryTest : IntegrationTest() {
+
+    @Test
+    fun `should search`() {
+    }
+
+}
