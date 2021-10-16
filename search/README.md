670fb7573d764dc70371b4984862b4e0467695d8
André Formento <andreformento.sc@gmail.com>
2021-10-16 09:40:05 -0300

    wip

---
+++ search/README.md
@@ -0,0 +1,3 @@
+# Search
+
+- [solr doc add-requesthandler](https://solr.apache.org/guide/8_3/config-api.html#examples-of-handler-and-component-commands)
--- search/src/main/kotlin/com/formento/search/SolrClientFactory.kt
+++ search/src/main/kotlin/com/formento/search/SolrClientFactory.kt
@@ -7,9 +7,29 @@ import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+
+import org.apache.solr.client.solrj.impl.CloudSolrClient
+import java.util.*
+
+
+data class SolrCollectionProperties(val name: String)
+data class SolrClusterProperties(val zkHosts: List<String>, val zkChroot: String?)
+data class SolrStandaloneProperties(val host: String, val port: Int)
+
 @ConfigurationProperties(prefix = "solr")
 @ConstructorBinding
 data class SolrProperties(val host: String, val port: Int)
+
+
+
+
+
+//data class SolrProperties(
+//    val collection: SolrCollectionProperties,
+//    val cluster: SolrClusterProperties?,
+//    val standalone: SolrStandaloneProperties?,
+//)
+
 
 @Configuration
 class SolrClientFactory {
@@ -19,3 +39,18 @@ class SolrClientFactory {
         Http2SolrClient.Builder("http://${solrProperties.host}:${solrProperties.port}/solr").build()
 
 }
+
+
+
+
+//fun createSolrClient(solrProperties: SolrProperties): SolrClient =
+//    if (solrProperties.standalone == null) {
+//        CloudSolrClient
+//            .Builder(solrProperties.cluster!!.zkHosts, Optional.ofNullable(solrProperties.cluster!!.zkChroot))
+//            .build()
+//            .also { it.connect() }
+//    } else {
+//        Http2SolrClient
+//            .Builder("http://${solrProperties.standalone.host}:${solrProperties.standalone.port}/solr")
+//            .build()
+//    }
