ba8d0f843f4caa570171f9560f16d1f45a757ad9
André Formento <andreformento.sc@gmail.com>
2021-10-25 12:04:41 -0300

    validate index

--- search/solr/setup.sh
+++ search/solr/setup.sh
@@ -19,22 +19,41 @@ wait_for_solr() {
   echo "Done! $SOLR_URL"
 }
 
+wait_for_document() {
+  SOLR_URL="$1/solr/products/select?indent=true&q.op=OR&q=*%3A*"
+
+  RETRY_COUNT=0
+  while [[ "$(curl ${SOLR_URL} | grep 'First')" ]]; do
+    if [ $RETRY_COUNT -gt 60 ]; then
+       echo "Oops! Documents not indexed. See details with 'docker-compose logs -f solr3'"
+       exit 1
+    fi
+
+    sleep 0.5
+    echo "retry $SOLR_URL $RETRY_COUNT"
+    ((RETRY_COUNT+=1))
+  done;
+
+  echo "Done! $SOLR_URL"
+}
+
+solr_host="http://solr3:8983"
 wait_for_solr "http://solr1:8983/solr/"
 wait_for_solr "http://solr2:8983/solr/"
-wait_for_solr "http://solr3:8983/solr/"
+wait_for_solr "${solr_host}/solr/"
 
-curl -X POST "http://solr3:8983/solr/admin/collections?action=CREATE&name=products&numShards=2&replicationFactor=1&wt=json"
+curl -X POST "${solr_host}/solr/admin/collections?action=CREATE&name=products&numShards=2&replicationFactor=1&wt=json"
 
-wait_for_solr "http://solr3:8983/solr/products/select?q.op=OR&q=*:*"
+wait_for_solr "${solr_host}/solr/products/select?q.op=OR&q=*:*"
 
 curl -X POST \
         -H 'Content-type:application/json' \
         -d @/solr/config/request-handler.json \
-        "http://solr3:8983/api/collections/products/config"
+        "${solr_host}/api/collections/products/config"
 
 curl -X POST \
         -H 'Content-type:application/json' \
         -d @/solr/sample/products.json \
-        "http://solr3:8983/solr/products/update?commit=true"
+        "${solr_host}/solr/products/update?commit=true"
 
-echo "Solr configured with success"
+wait_for_document "${solr_host}/solr/"
