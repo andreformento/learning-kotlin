5949c95fccd26d0a3a51d586fee81dd56c2df298
André Formento <andreformento.sc@gmail.com>
2021-10-25 13:46:07 -0300

    fix validation

--- search/solr/setup.sh
+++ search/solr/setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 wait_for_solr() {
-  SOLR_URL="$1"
+  SOLR_URL="$1/"
 
   RETRY_COUNT=0
   while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $SOLR_URL)" != "200" ]]; do
@@ -12,7 +12,7 @@ wait_for_solr() {
     fi
 
     sleep 0.5
-    echo "retry $SOLR_URL $RETRY_COUNT"
+    echo "waiting $SOLR_URL $RETRY_COUNT"
     ((RETRY_COUNT+=1))
   done;
 
@@ -20,40 +20,42 @@ wait_for_solr() {
 }
 
 wait_for_document() {
-  SOLR_URL="$1/solr/products/select?indent=true&q.op=OR&q=*%3A*"
+  SOLR_URL="$1/products/select?indent=true&q.op=OR&q=*%3A*"
 
   RETRY_COUNT=0
-  while [[ "$(curl ${SOLR_URL} | grep 'First')" ]]; do
-    if [ $RETRY_COUNT -gt 60 ]; then
+  while [[ "$(curl $SOLR_URL | grep -c 'First')" -le 0 ]]; do
+    if [ $RETRY_COUNT -gt 30 ]; then
        echo "Oops! Documents not indexed. See details with 'docker-compose logs -f solr3'"
        exit 1
     fi
 
     sleep 0.5
-    echo "retry $SOLR_URL $RETRY_COUNT"
+    echo "waiting documents $SOLR_URL $RETRY_COUNT"
     ((RETRY_COUNT+=1))
   done;
 
-  echo "Done! $SOLR_URL"
+  echo "Documents indexed $SOLR_URL with success"
 }
 
 solr_host="http://solr3:8983"
-wait_for_solr "http://solr1:8983/solr/"
-wait_for_solr "http://solr2:8983/solr/"
-wait_for_solr "${solr_host}/solr/"
-
-curl -X POST "${solr_host}/solr/admin/collections?action=CREATE&name=products&numShards=2&replicationFactor=1&wt=json"
-
-wait_for_solr "${solr_host}/solr/products/select?q.op=OR&q=*:*"
+wait_for_solr "http://solr1:8983/solr"
+wait_for_solr "http://solr2:8983/solr"
+wait_for_solr "${solr_host}/solr"
 
 curl -X POST \
-        -H 'Content-type:application/json' \
-        -d @/solr/config/request-handler.json \
-        "${solr_host}/api/collections/products/config"
+     -o /dev/null \
+     "${solr_host}/solr/admin/collections?action=CREATE&name=products&numShards=2&replicationFactor=1&wt=json"
 
 curl -X POST \
-        -H 'Content-type:application/json' \
-        -d @/solr/sample/products.json \
-        "${solr_host}/solr/products/update?commit=true"
+     -o /dev/null \
+     -H 'Content-type:application/json' \
+     -d @/solr/config/request-handler.json \
+     "${solr_host}/api/collections/products/config"
 
-wait_for_document "${solr_host}/solr/"
+curl -X POST \
+     -o /dev/null \
+     -H 'Content-type:application/json' \
+     -d @/solr/sample/products.json \
+     "${solr_host}/solr/products/update?commit=true"
+
+wait_for_document "${solr_host}/solr"
