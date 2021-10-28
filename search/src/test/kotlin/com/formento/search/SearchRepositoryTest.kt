40a202f5c12ec82570d7ce08691179c041a13a72
André Formento <andreformento.sc@gmail.com>
2021-10-28 15:22:42 -0300

    remove comments

--- search/src/test/kotlin/com/formento/search/SearchRepositoryTest.kt
+++ search/src/test/kotlin/com/formento/search/SearchRepositoryTest.kt
@@ -2,7 +2,6 @@ package com.formento.search
 
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.ClassRule
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -15,10 +14,7 @@ import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.containers.DockerComposeContainer
-import org.testcontainers.containers.SolrContainer
-import org.testcontainers.utility.DockerImageName
 import java.io.File
-
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,8 +23,7 @@ import java.io.File
 class SearchRepositoryTest {
 
     companion object {
-        private const val solrPort = 18983
-        private val instance: KDockerComposeContainer by lazy { defineDockerCompose()}
+        private val instance: KDockerComposeContainer by lazy { defineDockerCompose() }
 
         class KDockerComposeContainer(files: List<File>) : DockerComposeContainer<KDockerComposeContainer>(files)
 
@@ -39,19 +34,16 @@ class SearchRepositoryTest {
         @DynamicPropertySource
         @JvmStatic
         fun setProperties(registry: DynamicPropertyRegistry) {
-//            registry.add("solr.url") { "http://${instance.getContainerByServiceName()}:${IntegrationTest.solrContainer.solrPort}/solr" }
             val solrContainer = instance.getContainerByServiceName("solr_1").orElse(null)
 
             registry.add("solr.standalone.host") { solrContainer.containerIpAddress }
             registry.add("solr.standalone.port") { solrContainer.boundPortNumbers.first() }
-//            registry.add("solr.url") { "http://${solrContainer.host}:${solrContainer.boundPortNumbers.first()}/solr" }
         }
 
         @BeforeAll
         @JvmStatic
         internal fun beforeAll() {
             instance.start()
-//            println(instance.getContainerByServiceName("solr_1").orElse(null).boundPortNumbers.first())
         }
 
         @AfterAll
@@ -60,38 +52,6 @@ class SearchRepositoryTest {
             instance.stop()
         }
     }
-
-
-//    companion object {
-//
-//        @ClassRule
-//        val solrContainer = SolrContainer(DockerImageName.parse("solr:8.10.0"))
-//
-////        @ClassRule
-////        var environment: DockerComposeContainer<KDockerComposeContainer> =
-////            DockerComposeContainer(File("docker-compose.yml"))
-////                .withExposedService("redis_1", REDIS_PORT)
-////                .withExposedService("elasticsearch_1", ELASTICSEARCH_PORT)
-//
-//        @DynamicPropertySource
-//        @JvmStatic
-//        fun setProperties(registry: DynamicPropertyRegistry) {
-//            registry.add("solr.url") { "http://${solrContainer.containerIpAddress}:${solrContainer.solrPort}/solr" }
-//        }
-//
-//        @BeforeAll
-//        @JvmStatic
-//        internal fun beforeAll() {
-//            solrContainer.start()
-//        }
-//
-//        @AfterAll
-//        @JvmStatic
-//        internal fun afterAll() {
-//            solrContainer.close()
-//        }
-//    }
-
 
     @Autowired
     lateinit var repo: Products
