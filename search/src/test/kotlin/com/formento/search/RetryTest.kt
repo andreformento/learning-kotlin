2633ed554a26444a62019da8bca7ac746e2d6f7e
André Formento <andreformento.sc@gmail.com>
2021-10-28 16:31:47 -0300

    test

--- search/src/test/kotlin/com/formento/search/RetryTest.kt
+++ search/src/test/kotlin/com/formento/search/RetryTest.kt
@@ -12,12 +12,19 @@ internal class RetryTest {
 
     @Test
     fun shouldRunOnTheSecondTime() {
+//        var secondTime = false
+//
+//        val function: () -> Boolean = {
+//            val result = secondTime
+//            secondTime = true
+//            result
+//        }
+
         var attemptCount = 0
 
-//        val function = () -> {
-//            attemptCount == 1
-//        }
-        // TODO
-        assertThat(Retry(sleepTime = Duration.ZERO, tryAtLeast = 1).tryToRun { true }).isTrue
+        val function: () -> Boolean = { ++attemptCount == 1 }
+
+        assertThat(Retry(sleepTime = Duration.ZERO, tryAtLeast = 1).tryToRun(function)).isTrue
     }
+
 }
